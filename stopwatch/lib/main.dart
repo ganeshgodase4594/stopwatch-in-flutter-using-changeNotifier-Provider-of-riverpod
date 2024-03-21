23aa93264a71050d612e72e1ca82e33531a84803
Ganesh Godase <godaseganesh4594@gmail.com>
2024-03-21 19:20:46 +0530

    stopwatch

---
+++ stopwatch/lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Stopwatch stopwatch;
+  late Timer t;
+
+  void handleStartStop() {
+    if (stopwatch.isRunning) {
+      stopwatch.stop();
+    } else {
+      stopwatch.start();
+    }
+  }
+
+  String returnFormattedText() {
+    var milli = stopwatch.elapsed.inMilliseconds;
+
+    String milliseconds = (milli % 1000).toString().padLeft(3, "0");
+    String seconds = ((milli ~/ 1000) % 60).toString().padLeft(2, "0");
+    String minutes = ((milli ~/ 1000) ~/ 60).toString().padLeft(2, "0");
+
+    return "$minutes:$seconds:$milliseconds";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    stopwatch = Stopwatch();
+
+    t = Timer.periodic(const Duration(milliseconds: 30), (timer) {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CupertinoButton(
+                onPressed: () {
+                  handleStartStop();
+                },
+                padding: const EdgeInsets.all(0),
+                child: Container(
+                  height: 250,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: const Color(0xff0395eb),
+                      width: 4,
+                    ),
+                  ),
+                  child: AnimatedOpacity(
+                    opacity: stopwatch.isRunning ? 1.0 : 0.5,
+                    duration: const Duration(milliseconds: 500),
+                    child: Text(
+                      returnFormattedText(),
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CupertinoButton(
+                onPressed: () {
+                  stopwatch.reset();
+                },
+                padding: const EdgeInsets.all(0),
+                child: const Text(
+                  "Reset",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
